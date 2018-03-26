92b683c39d9b2ddd8f2bd256ade96fed916d0b38
Administrator <galaxyqiang@gmail.com>
2018-03-27 05:24:30 +0800

    添加fragment测试

--- app/src/main/java/com/john/history/history/ui/FragmentActivity.java
+++ app/src/main/java/com/john/history/history/ui/FragmentActivity.java
@@ -1,6 +1,5 @@
 package com.john.history.history.ui;
 
-import android.app.Activity;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,23 +8,49 @@ import android.util.Log;
 import com.john.history.history.R;
 import com.john.history.history.bean.SingleInstance;
 import com.john.history.history.fragment.FragmentA;
+import com.john.history.history.fragment.FragmentB;
 
-public class FragmentActivity extends AppCompatActivity implements FragmentA.OnFragmentInteractionListener {
+public class FragmentActivity extends AppCompatActivity implements FragmentA.OnFragmentInteractionListener ,FragmentB.OnFragmentInteractionListener{
+    private String LOG_TAG="FragmentActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        FragmentA fragmentA;
-        //单例持有activity的引用
-
-        SingleInstance.getInstance().setmContext(this,5);
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment);
-        fragmentA=new FragmentA();
-        getSupportFragmentManager().beginTransaction().replace(R.id.content,fragmentA).commit();
 
-//        LeakThread leakThread = new LeakThread();
-//        leakThread.start();
+        addFragment();
+    }
+
+    private void addFragment() {
+        FragmentA fragmentA=new FragmentA();
+        getSupportFragmentManager().beginTransaction().add(R.id.content,fragmentA).commit();
+    }
+
+    private void leakContext() {
+        //单例类的静态变量持有activity的引用，在activity销毁的时候造成内存泄露
+        SingleInstance.getInstance().setmContext(this,5);
+    }
+
+    private void testFragment() {
+        FragmentA fragmentA;
+        FragmentB fragmentB;
+        fragmentA=new FragmentA();
+        fragmentB=new FragmentB();
+
+        android.support.v4.app.FragmentManager fm=getSupportFragmentManager();
+        /*fm.findFragmentById()的参数不仅可以是静态Fragment的id，也可以是已经动态加载完毕后Fragment的id
+        * fm.findFragmentByTag()，必须等任务完成后才能根据TAG标签找到Fragment，如果希望任务立刻被执行可以调用
+        * fm.executePendingTransactions()方法*/
+        fm.beginTransaction().add(R.id.content,fragmentA,"FragmentA").addToBackStack("FragmentA").commit();
+        fm.executePendingTransactions();
+        Log.d("aaa",String.valueOf(fm.findFragmentById(R.id.content)));
+        Log.d("aaa",fm.findFragmentByTag("FragmentA").toString());
+
+        fm.beginTransaction().add(R.id.content,fragmentB,"FragmentB").addToBackStack("FragmentB").commit();
+        fm.executePendingTransactions();
+        Log.d("aaa",String.valueOf(fm.findFragmentById(R.id.content)));
+        Log.d("aaa",fm.findFragmentByTag("FragmentB").toString());
     }
 
     @Override
@@ -36,28 +61,29 @@ public class FragmentActivity extends AppCompatActivity implements FragmentA.OnF
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d("xxx","onStop");
+        Log.d(LOG_TAG,"onStop");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d("xxx","onDestroy");
+        Log.d(LOG_TAG,"onDestroy");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d("xxx","onPause");
+        Log.d(LOG_TAG,"onPause");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d("xxx","F"+Boolean.toString(((Activity)SingleInstance.getInstance().mContext).isFinishing()));
+
+//        Log.d(LOG_TAG,"F"+Boolean.toString(((Activity)SingleInstance.getInstance().mContext).isFinishing()));
     }
 
-    //非静态内部类持有外部类的引用
+    //非静态内部类持有外部类的引用，当activity在6分钟内退出，由于LeakThread拥有activity的引用，任务没有执行完毕造成内存泄露
     class LeakThread extends Thread {
         @Override
         public void run() {
--- app/src/main/java/com/john/history/history/ui/MainActivity.java
+++ app/src/main/java/com/john/history/history/ui/MainActivity.java
@@ -2,11 +2,21 @@ package com.john.history.history.ui;
 
 import android.arch.lifecycle.LifecycleActivity;
 import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.ViewModelProvider;
+import android.content.Context;
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.storage.StorageManager;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.john.history.history.R;
@@ -15,12 +25,14 @@ import com.john.history.history.bean.Name;
 import com.john.history.history.database.Itemyyy;
 import com.john.history.history.databinding.ActivityMainBinding;
 
+import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 //import butterknife.BindView;
 //import butterknife.ButterKnife;
 //import butterknife.OnClick;
+
 
 public class MainActivity extends AppCompatActivity {
     private int i;
@@ -54,6 +66,10 @@ public class MainActivity extends AppCompatActivity {
         for(int a :list){
 
         }
+        Log.d("xxx",getExternalCacheDir().toString());
+        Log.d("xxx", Environment.getExternalStorageDirectory().toString());
+        Log.d("xxx", Environment.getDataDirectory().toString());
+
 
 //        new Thread(() -> {
 //            name.firstname = "qiang";
@@ -91,6 +107,31 @@ public class MainActivity extends AppCompatActivity {
 
         bbb.postValue(new Itemyyy(date2string(mDate)+i));
         i+=1;
+    }
+    private void accessStorage(){
+        StorageManager sm = (StorageManager) getSystemService(Context.STORAGE_SERVICE);
+        Intent intent = sm.getPrimaryStorageVolume().createAccessIntent(Environment.DIRECTORY_PICTURES);
+        startActivityForResult(intent, 10);
+
+    }
+
+  /*调用相机的拍照功能，并将相片保存到本应用的外部私有文件夹中
+    由于相机的activity要访问本应用外部存储中的私有文件夹中的文件，在7.0之后必须使用FileProvider
+    来提供访问
+  * */
+    private void savePic2ExternalCache(){
+        File file=getExternalCacheDir();
+        File mFile=new File(file,"John");
+        mFile.mkdirs();
+        File file1=new File(mFile,"home.jpg");
+
+        Uri contentUri= FileProvider.getUriForFile(this,"com.john.history.history",file1);
+        Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, contentUri);
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION
+                | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+
+        startActivityForResult(intent, 100);
     }
 
     private String date2string(Date date){
